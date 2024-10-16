658192947fcd88e168ea526906b8124ec76319f7
Muikia Benson <benmuigai20@gmail.com>
2024-10-16 13:30:02 +0300

    Add route to create a new notebook

--- backend/services/notebookService/routes/notebookRoutes.js
+++ backend/services/notebookService/routes/notebookRoutes.js
@@ -3,3 +3,8 @@ const router = express.Router();
 const notebookController = require('../controllers/notebookController');
 const auth = require('../middlewares/auth');
 
+// Notebook routes
+router.post('/notebooks', auth, notebookController.createNotebook);
+router.get('/notebooks', auth, notebookController.getNotebooks);
+router.put('/notebooks/:notebookId/rename', auth, notebookController.renameNotebook);
+router.delete('/notebooks/:notebookId', auth, notebookController.deleteNotebook);
