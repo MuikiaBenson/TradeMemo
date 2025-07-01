25e32bc30543a6078c893eb4289514c968d7b90d
Muikia Benson <benmuigai20@gmail.com>
2025-07-01 12:33:42 +0300

    Load core libraries
    express: Framework to create API endpoints.
    
    mongoose: ORM to connect and interact with MongoDB.
    
    dotenv: Loads .env file variables (database URI, JWT secret).
    
    cors: Allows cross-origin requests.
    
    connectDB: A custom function that connects MongoDB.

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+const app = express();
+// Middlewares
+// Routes
+// Connect DB and Start Server
