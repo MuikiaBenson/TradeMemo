811bf1ff35cded2157876af478bbb38d1bb87342
Muikia Benson <benmuigai20@gmail.com>
2025-07-03 11:17:39 +0300

    Add password hashing before saving and compare the hashed password

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,7 @@
 // Import mongoose for schema and model creation
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 
 // Define the user schema structure
 const userSchema = new mongoose.Schema({
@@ -33,6 +35,20 @@ const userSchema = new mongoose.Schema({
 }, {
   versionKey: false          // Optional: disables the `__v` version field
 });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare hashed password method
+userSchema.methods.comparePassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 // Export the User model
 module.exports = mongoose.model('User', userSchema);
